2dfba9ea41fb549ce57322326ac750387608b1ce
whopper-wrapper <108198496+whopper-wrapper@users.noreply.github.com>
2022-06-26 07:48:35 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-echo "Hello from index file";
+echo "Test phpinfo.";
 ?>
 <br>
-<a href="server/phpinfo.php">View phpinfo</a><br>
-<a href="server/a_directory">Go to a sub directory</a>
+<a href="test/info/phpinfo.php">View phpinfo</a><br>
