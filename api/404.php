89c930881dedea1df42eb649265d699e580f0e37
whopper-wrapper <108198496+whopper-wrapper@users.noreply.github.com>
2022-06-26 09:12:19 +0900

    Delete 404.php

--- api/404.php
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<html>
-  <head><title>404 Not Found</title></head>
-  <body>
-    <h1>Not Found</h1>
-    <p>The requested resource <code class="url"><?= $_SERVER['REQUEST_URI'] ?></code> was not found on this server.</p>
-  </body>
-</html>
