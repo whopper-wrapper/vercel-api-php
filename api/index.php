2e01eb029408e658c529848664931d2637a6a8a3
whopper-wrapper <108198496+whopper-wrapper@users.noreply.github.com>
2022-06-26 09:14:16 +0900

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,18 +1,2 @@
 <?php
-$reqUrl = parse_url($_SERVER['REQUEST_URI']);
-
-if(isset($reqUrl['path'])){
-  $file = $reqUrl['path'];
-  if($file == '/'){
-    $file = '/index';
-  }
-} else {
-  $file = '/index';
-}
-
-if(!file_exists(__DIR__.'/..'.$file.'.php')){
-  include '404.php';
-  exit;
-} else {
-  require_once __DIR__.'/..'.$file.'.php';
-}
+    echo 'Hello World';
