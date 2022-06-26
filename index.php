0c1225814aafe77d21ef2f8bbdd4093fa4c57e68
whopper-wrapper <108198496+whopper-wrapper@users.noreply.github.com>
2022-06-26 09:34:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 ?>
 <br>
 <a href="api/">View api</a><br>
-<a href="api/functions/">View phpinfo</a><br>
+<a href="api/functions/">View functions</a><br>
 <a href="test/">View test</a><br>
 <a href="test/info/phpinfo.php">View phpinfo</a><br>
